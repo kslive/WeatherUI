0500d62a9e6f1bea98a0c77bae9f96bc88fc3422
Eugene <Kiselev>
2021-11-27 20:54:35 +0300

    Initial Commit

---
+++ WeatherUI/WeatherUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherUIApp.swift
+//  WeatherUI
+//
+//  Created by Eugene Kiselev on 27.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct WeatherUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
